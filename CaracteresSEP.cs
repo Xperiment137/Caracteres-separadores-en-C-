e082bbfe22e8d7260093b1432c803f4d8703bb99
muerteloca12 <43545742+muerteloca12@users.noreply.github.com>
2020-04-13 23:07:46 +0200

    Create CaracteresSEP.cs

---
+++ CaracteresSEP.cs
@@ -0,0 +1,74 @@
+// string text = " hola amigo  & 1-si & 2- no  # muy bien todo & hola otra vez ";
+
+void InicioOpciones(int op)
+    {
+        string Fichero_Stri = fichero.text;
+        char[] delimiterChars = { '#', '&' };
+        string[] words = Fichero_Stri.Split(delimiterChars);
+        int p = 0;
+        int j = 0;
+        string[] textos = new string[3];
+        for (int t = 0; t < op; t++)
+        {
+
+            foreach (var word in words)
+            {
+                bool re = word.Contains(t + "-");
+
+                if (re)
+                {
+                    textos[p++] = ($"<{word}>");
+
+
+                }
+            }
+
+            foreach (var word in words)
+            {
+                bool re = word.Contains("-");
+
+                if (!re)
+                {
+                    textos[p++] = ($"<{word}>");
+
+
+                }
+            }
+
+        }
+    }
+
+    void InicioTextos()
+    {
+       
+       
+        char[] delimiterChars = { '#', '&' };
+        string Fichero_Stri = fichero.text;
+        string[] words = Fichero_Stri.Split(delimiterChars);
+        int p = 0;
+        int j = 0;
+        foreach (var word in words)
+        {
+            bool re = word.Contains("-");
+            if (!re)
+            {
+                j++;
+            }
+        }
+        string[] textos = new string[j];
+
+
+        foreach (var word in words)
+        {
+            bool re = word.Contains("-");
+
+            if (!re)
+            {
+                textos[p++] = ($"<{word}>");
+
+
+            }
+        }
+
+    }
+}
